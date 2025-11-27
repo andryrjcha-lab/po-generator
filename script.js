9933e28401e65f8a2350d33277aee73ceb4839a3
andryrjcha-lab <andryrj.cha@gmail.com>
2025-11-27 18:32:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,98 +1,102 @@
 let items = [];
 
-function addItem(){
-  items.push({
-    desc:"",
-    material:"",
-    qty:"",
-    unit:"",
-    price:""
-  });
-  renderItems();
-}
-
-function renderItems(){
-  let tbody = document.getElementById("itemsBody");
-  tbody.innerHTML = "";
-  items.forEach((it, i)=>{
-    tbody.innerHTML += `
-      <tr>
-        <td>${i+1}</td>
-        <td><input class="itemInput" data-i="${i}" data-f="desc" value="${it.desc}"></td>
-        <td><input class="itemInput" data-i="${i}" data-f="material" value="${it.material}"></td>
-        <td><input class="itemInput" data-i="${i}" data-f="qty" value="${it.qty}"></td>
-        <td><input class="itemInput" data-i="${i}" data-f="unit" value="${it.unit}"></td>
-        <td><input class="itemInput" data-i="${i}" data-f="price" value="${it.price}"></td>
-      </tr>`;
-  });
-
-  document.querySelectorAll(".itemInput").forEach(inp=>{
-    inp.addEventListener("input", e=>{
-      let i = e.target.dataset.i;
-      let f = e.target.dataset.f;
-      items[i][f] = e.target.value;
-      updatePreview();
+function addItem() {
+    items.push({
+        desc: "",
+        material: "",
+        qty: "",
+        unit: "",
+        price: ""
     });
-  });
+    renderItems();
 }
 
-function updatePreview(){
-  document.getElementById("p_poNo").innerText = poNo.value;
-  document.getElementById("p_toCompany").innerText = toCompany.value;
-  document.getElementById("p_toUP").innerText = toUP.value;
-  document.getElementById("p_toAddress").innerText = toAddress.value;
-  document.getElementById("p_siteId").innerText = siteId.value;
-  document.getElementById("p_siteName").innerText = siteName.value;
-  document.getElementById("p_currency").innerText = currency.value;
+function renderItems() {
+    let tbody = document.getElementById("itemsBody");
+    tbody.innerHTML = "";
 
-  let today = new Date().toLocaleDateString("id-ID");
-  p_date.innerText = today;
-  p_date2.innerText = today;
+    items.forEach((item, i) => {
+        tbody.innerHTML += `
+        <tr>
+            <td>${i + 1}</td>
+            <td><input class="itemInput" data-i="${i}" data-f="desc" value="${item.desc}"></td>
+            <td><input class="itemInput" data-i="${i}" data-f="material" value="${item.material}"></td>
+            <td><input class="itemInput" data-i="${i}" data-f="qty" value="${item.qty}"></td>
+            <td><input class="itemInput" data-i="${i}" data-f="unit" value="${item.unit}"></td>
+            <td><input class="itemInput" data-i="${i}" data-f="price" value="${item.price}"></td>
+        </tr>
+        `;
+    });
 
-  let tbody = document.getElementById("itemsBody");
-  tbody.innerHTML = "";
-  items.forEach((it, i)=>{
-    tbody.innerHTML += `
-    <tr>
-      <td>${i+1}</td>
-      <td>${it.desc}</td>
-      <td>${it.material}</td>
-      <td>${it.qty}</td>
-      <td>${it.unit}</td>
-      <td>${formatRupiah(it.price)}</td>
-    </tr>`;
-  });
+    document.querySelectorAll(".itemInput").forEach(inp => {
+        inp.addEventListener("input", e => {
+            let i = e.target.dataset.i;
+            let f = e.target.dataset.f;
+            items[i][f] = e.target.value;
+            updatePreview();
+        });
+    });
 }
 
-function formatRupiah(x){
-  if(!x) return "";
-  return parseFloat(x).toLocaleString("id-ID");
+function updatePreview() {
+    p_poNo.innerText = poNo.value;
+    p_toCompany.innerText = toCompany.value;
+    p_toUP.innerText = toUP.value;
+    p_toAddress.innerText = toAddress.value;
+    p_siteId.innerText = siteId.value;
+    p_siteName.innerText = siteName.value;
+    p_currency.innerText = currency.value;
+
+    const today = new Date().toLocaleDateString("id-ID");
+    p_date.innerText = today;
+    p_date2.innerText = today;
+
+    let tbody = document.getElementById("itemsBody");
+    tbody.innerHTML = "";
+
+    items.forEach((item, i) => {
+        tbody.innerHTML += `
+        <tr>
+            <td>${i + 1}</td>
+            <td>${item.desc}</td>
+            <td>${item.material}</td>
+            <td>${item.qty}</td>
+            <td>${item.unit}</td>
+            <td>${formatRupiah(item.price)}</td>
+        </tr>
+        `;
+    });
 }
 
-logoUpload.onchange = e=>{
-  let file = e.target.files[0];
-  let reader = new FileReader();
-  reader.onload = ()=> logoImg.src = reader.result;
-  reader.readAsDataURL(file);
+function formatRupiah(x) {
+    if (!x) return "";
+    return parseFloat(x).toLocaleString("id-ID");
+}
+
+logoUpload.onchange = e => {
+    let file = e.target.files[0];
+    let r = new FileReader();
+    r.onload = () => logoImg.src = r.result;
+    r.readAsDataURL(file);
 };
 
-signUpload.onchange = e=>{
-  let file = e.target.files[0];
-  let reader = new FileReader();
-  reader.onload = ()=> signImg.src = reader.result;
-  reader.readAsDataURL(file);
+signUpload.onchange = e => {
+    let file = e.target.files[0];
+    let r = new FileReader();
+    r.onload = () => signImg.src = r.result;
+    r.readAsDataURL(file);
 };
 
-function generatePDF(){
-  let element = document.getElementById("poPreview");
+function generatePDF() {
+    let element = document.getElementById("poPreview");
 
-  let opt = {
-    margin: 0,
-    filename: (poNo.value || "PO-RDG") + ".pdf",
-    image: { type: "jpeg", quality: 1 },
-    html2canvas: { scale: 2, useCORS: true, scrollY: 0 },
-    jsPDF: { unit: "mm", format: "a4", orientation: "portrait" }
-  };
+    let opt = {
+        margin: 0,
+        filename: (poNo.value || "PO-RDG") + ".pdf",
+        image: { type: "jpeg", quality: 1 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" }
+    };
 
-  html2pdf().set(opt).from(element).save();
+    html2pdf().set(opt).from(element).save();
 }
